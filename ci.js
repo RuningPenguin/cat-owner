e1edcf2ce55ef7d2e02781d8c9d875e8663f61ec
wangxu <xu.wang@nx-engine.com>
2023-01-06 23:36:52 +0800

    test build 05

--- ci.js
+++ ci.js
@@ -1,24 +1,25 @@
 const argv = process.argv;
 const ci = require("miniprogram-ci");
 const { version } = require("./package.json");
-const info = {
-	appid: "wxe01f582812d9d742",
-	desc: "编译",
-	robot: "default"
-};
+const appid = "wxe01f582812d9d742";
+const info = { desc: "编译", robot: 30 };
 
 argv.forEach(str => {
 	if (str.includes("desc")) {
 		info.desc = str.split("=")[1];
 	}
 	if (str.includes("robot")) {
-		info.robot = str.split("=")[1];
+		const robotMap = {
+			dev: 1,
+			uat: 2,
+			prod: 3
+		};
+		info.robot = robotMap[str.split("=")[1]];
 	}
 });
 
-
 const project = new ci.Project({
-	appid: info.appid,
+	appid,
 	type: "miniProgram",
 	projectPath: "./dist/build/mp-weixin",
 	privateKeyPath: "./ci-upload.key",
@@ -26,18 +27,49 @@ const project = new ci.Project({
 });
 
 async function upload() {
-	console.log(info)
-// 	await ci.upload({
-// 		...info,
-// 		project,
-// 		version,
-// 		onProgressUpdate: console.log,
-// 		setting: {
-// 			es7: true,
-// 			minify: true,
-// 			autoPrefixWXSS: true
-// 		}
-// 	});
+	await ci.upload({
+		...info,
+		project,
+		version,
+		onProgressUpdate: console.log,
+		pagePath: "pages/index/index",
+		setting: {
+			es7: true,
+			minify: true,
+			autoPrefixWXSS: true
+		}
+	});
 }
 
-upload();
+async function preview() {
+	await ci.preview({
+		...info,
+		project,
+		version,
+		onProgressUpdate: console.log,
+		pagePath: "pages/index/index", // 预览页面
+		// searchQuery: 'a=1&b=2',  // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
+		setting: {
+			es7: true,
+			minify: true,
+			autoPrefixWXSS: true
+		}
+	});
+}
+
+;(async function() {
+	switch (info.robot) {
+		case 1:
+			await preview();
+			break;
+		case 2:
+			await preview();
+			break;
+		case 3:
+			await upload();
+			break;
+		default:
+			console.log("using robot: dev|uat|prod");
+			break;
+	}
+})();
