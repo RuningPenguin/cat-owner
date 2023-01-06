44d63ff701d2c739f6c941e2595bc722bf98a8d9
wangxu <xu.wang@nx-engine.com>
2023-01-06 22:21:24 +0800

    test build 04

--- ci.js
+++ ci.js
@@ -1,25 +1,35 @@
+const argv = process.argv;
 const ci = require("miniprogram-ci");
-
 const { version } = require("./package.json");
-const appid = "wxe01f582812d9d742";
+const info = {
+	appid: "wxe01f582812d9d742",
+	desc: "编译",
+	robot: "default"
+};
 
-const desc = '编译1'
+argv.forEach(str => {
+	if (str.includes("desc")) {
+		info.desc = str.split("=")[1];
+	}
+	if (str.includes("robot")) {
+		info.robot = str.split("=")[1];
+	}
+});
 
 
 const project = new ci.Project({
-	appid,
+	appid: info.appid,
 	type: "miniProgram",
 	projectPath: "./dist/build/mp-weixin",
 	privateKeyPath: "./ci-upload.key",
 	ignores: ["node_modules/**/*", "src/**/*"]
 });
 
-async function upload({ robot }) {
+async function upload() {
 	await ci.upload({
+		...info,
 		project,
 		version,
-		desc,
-		robot,
 		onProgressUpdate: console.log,
 		setting: {
 			es7: true,
@@ -29,4 +39,4 @@ async function upload({ robot }) {
 	});
 }
 
-upload({ robot: 1 });
+upload();
